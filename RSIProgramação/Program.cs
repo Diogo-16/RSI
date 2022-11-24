80231a8b8bb062897b68085eb76f7689b607e778
Diogo-16 <68477566+Diogo-16@users.noreply.github.com>
2022-11-24 00:07:48 +0000

    RSI Programação C# 2022

---
+++ RSIProgramação/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Authentication;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        double ph;
+        int quantidade;
+
+        Console.WriteLine("Quantas soluções tem?");
+        quantidade = Convert.ToInt32(Console.ReadLine());
+        
+        while (quantidade <= 0)
+        {
+            Console.WriteLine("O valor da quantidade de soluções tem de ser positivo: ");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int i = 1; i <= quantidade; i++)
+        {
+            Console.WriteLine("------------------------\nIntroduza o seu pH: ");
+            ph = Convert.ToDouble(Console.ReadLine());
+
+            while (ph > 14 || ph < 0)
+            {
+                Console.WriteLine("Introduza um pH entre 0 e 14: ");
+                ph = Convert.ToDouble(Console.ReadLine());
+            }
+
+            if (ph < 7 && ph >= 0)
+            {
+                Console.WriteLine("A sua solução é ácida.\n------------------------");
+            }
+            else if (ph == 7)
+            {
+                Console.WriteLine("A sua solução é neutra.\n------------------------");
+            }
+            else if (ph > 7 && ph <= 14)
+            {
+                Console.WriteLine("A sua solução é alcalina.\n------------------------");
+            }
+        }
+        Console.ReadKey();
+    }
+}
